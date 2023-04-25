cae2ffe65b7557c19679cd13f5837d274d2d79f3
gusrodrigues <gus.201334@gmail.com>
2023-04-25 09:16:27 -0300

    Rotas

--- App.js
+++ App.js
@@ -1,29 +1,161 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { withTheme } from 'react-native-paper';
 
-import Login from './componentes/Login';
-import Home from './componentes/Home';
 
-function HomeScreen() {
+const Stack = createNativeStackNavigator();
+
+function TelaInicial({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+        <Button
+          title="ENTRAR"
+          onPress={() => navigation.navigate('Details')}
+        />
+        <Button
+          title="ENTRAR COM FACEBOOK"
+          onPress={() => navigation.navigate('Details')}
+        />
+        <Button
+          title="ENTRAR COM GOOGLE"
+          onPress={() => navigation.navigate('Details')}
+        />
+        <Button
+          title="Cadastro pessoal"
+          onPress={() => navigation.navigate('CadatroPessoal')}
+        />
+        <Button
+          title="Cadastro de animais"
+          onPress={() => navigation.navigate('CadatroAnimais')}
+        />
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
+function CadatroPessoal({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          title="ENTRAR"
+          onPress={() => navigation.navigate('TelaInicial')}
+        />
+        <input
+          id={id}
+          type={type}
+          className="w-full p-5 font-medium border rounded-md border-slate-300 placeholder:opacity-60"
+          placeholder={placeholder}
+          {...register(label, {
+            required: {
+              value: true,
+              message: 'required',
+            },
+          })}
+        />
+    </View>
+  );
+}
 
-function App() {
+function CadatroAnimais({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          title="ENTRAR"
+          onPress={() => navigation.navigate('Details')}
+        />
+        <input
+          id={id}
+          type={type}
+          className="w-full p-5 font-medium border rounded-md border-slate-300 placeholder:opacity-60"
+          placeholder={placeholder}
+          {...register(label, {
+            required: {
+              value: true,
+              message: 'required',
+            },
+          })}
+        />
+    </View>
+  );
+}
+
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="/" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={TelaInicial}
+          options={{
+            title: 'Login',
+            headerStyle: {
+              backgroundColor: '#cfe9e5',
+            },
+            headerTintColor: '#434343',
+            headerTitleStyle: {
+              fontFamily: 'Roboto Medium',
+              fontWeight: 'bold',
+            },
+            headerLeft: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="menu"
+                color="#434343"
+                headerBackImageSource="menu"
+              />
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="Cadatro Pessoal"
+          component={CadatroPessoal}
+          options={{
+            title: 'Login',
+            headerStyle: {
+              backgroundColor: '#cfe9e5',
+            },
+            headerTintColor: '#434343',
+            headerTitleStyle: {
+              fontFamily: 'Roboto Medium',
+              fontWeight: 'bold',
+            },
+            headerLeft: () => (
+              <Button
+                onPress={() => navigation.navigate('CadatroPessoal')}
+                title="menu"
+                color="#434343"
+                headerBackImageSource="menu"
+              />
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="Cadatro Animais"
+          component={CadatroAnimais}
+          options={{
+            title: 'Login',
+            headerStyle: {
+              backgroundColor: '#cfe9e5',
+            },
+            headerTintColor: '#434343',
+            headerTitleStyle: {
+              fontFamily: 'Roboto Medium',
+              fontWeight: 'bold',
+            },
+            headerLeft: () => (
+              <Button
+                onPress={() => navigation.navigate('CadatroAnimais')}
+                title="menu"
+                color="#434343"
+                headerBackImageSource="menu"
+              />
+            ),
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;
--- componentes/Navbar.js
+++ componentes/Navbar.js
@@ -1,31 +1,24 @@
-import { StyleSheet } from 'react-native';
+import Navbar from 'react-bootstrap/Navbar';
+import { View, Text } from 'react-native';
 
-export default function Navbar({}) {
-    return (
-        <div className="pos-f-t">
-            <nav className="navbar fixed-top" style={navbar_styles.navbar}>
-                <button className="navbar-toggler" type="button">
-                <span className="navbar-toggler-icon" style={navbar_styles.icon}></span>
-                </button>
-            </nav>
-        </div>
-    );
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+export default function NavbarHome() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Stack.Navigator>
+            <Navbar bg="light" variant="light">
+                <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
+        </Stack.Navigator>
+    </View>
+  );
 }
-
-const navbar_styles = StyleSheet.create({
-    navbar: {
-        width: 360,
-        height: 56,
-        backgroundColor: '#88c9bf',
-        marginBottom: 60,
-        
-    },
-    icon: {
-        width: 24,
-        height: 24,
-        fontSize: 15,
-        paddingLeft: 16,
-        paddingTop: 16,
-        paddingBottom: 16,
-    },
-});
